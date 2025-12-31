bf2c2782cb00d491a0c7b66eee598f3db86037e4
Wesley Arruda <felipeoarruda@hotmail.com>
2025-12-31 18:29:56 -0300

    feat: :sparkles: add: Adicionar modelo Evento

---
+++ src/test/models/evento.test.js
@@ -0,0 +1,19 @@
+import {
+  describe, expect, it,
+} from '@jest/globals';
+import Evento from '../../models/evento.js';
+
+describe('Testando o modelo Evento', () => {
+  const objetoEvento = {
+    nome: 'Evento Teste',
+    descricao: 'descrição do evento teste',
+    data: '2023-01-01',
+    autor_id: 1,
+  };
+  it('Deve instanciar um novo Evento', () => {
+    const evento = new Evento(objetoEvento);
+    expect(evento).toEqual(
+      expect.objectContaining(objetoEvento),
+    );
+  });
+});
